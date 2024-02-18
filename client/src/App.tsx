63b3604aa955fed5b891b23c87f1e69fee9eb339
sitaram.oli <sitaram.oli@cedargate.com>
2024-02-18 13:39:05 +0545

    Pages and Routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import SignIn from "./pages/SignIn.tsx";
+
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
