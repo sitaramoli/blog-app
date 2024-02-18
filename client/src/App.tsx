7ea204c281b0d2828de55f0fbd509e796a18b30a
sitaram.oli <sitaram.oli@cedargate.com>
2024-02-18 16:16:51 +0545

    Header section added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,13 @@ import Projects from "./pages/Projects.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import SignIn from "./pages/SignIn.tsx";
+import Header from "./components/Header.tsx";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
